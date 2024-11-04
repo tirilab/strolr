728b3e1c8d14e3a8244830fd89bb7630646f8bfb
Michelle Nguyen <68311669+mh-n@users.noreply.github.com>
2024-11-04 18:48:45 -0500

    Update strolr_amia.py

--- strolr_amia.py
+++ strolr_amia.py
@@ -234,11 +234,10 @@ if user_input:
                 # Send user's question to our chain
                 context = "\n".join([message["content"] for message in st.session_state.messages])
                 result = chain.invoke(query)
-                print(type(result))
                 metadata = [msg for msg in result]
-                print(metadata[0][1])
+            
                 response = metadata[0][1]
-                #response = format_response(result.pretty_print())
+                response = format_response(response)
                 if ("don't know" in response) or ("do not know" in response) or ("cannot answer" in response) or ("can't answer" in response):
                     response = re.sub(r"(Sources used:.*)", '', response, flags=re.DOTALL)
                 # Simulate stream of response with milliseconds delay
